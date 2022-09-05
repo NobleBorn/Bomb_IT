fa14c7c26120356fe1370f81f4d5987de2ea373a
Snapz <max.e.levin@gmail.com>
2022-09-05 11:41:29 +0200

    Skapa klasser

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
---
+++ core/src/com/mygdx/game/Wall.java
@@ -0,0 +1,4 @@
+package com.mygdx.game;
+
+public class Wall implements HavePosition{
+}
