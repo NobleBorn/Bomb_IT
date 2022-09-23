98504da6f8ea04bf321aad81d9f63227676391db
NobleBorn <00mojali.afg@gmail.com>
2022-09-23 11:02:54 +0200

    Connecting menuScreen and gameScreen

---
+++ core/src/Controllers/MenuScreenController.java
@@ -0,0 +1,8 @@
+package Controllers;
+import com.mygdx.game.*;
+import Views.*;
+
+public class MenuScreenController {
+    private MainMenuScreen mainMenuScreen;
+    private MenuScreenView menuScreenView;
+}
---
+++ core/src/Views/MenuScreenView.java
@@ -0,0 +1,7 @@
+package Views;
+import com.mygdx.game.*;
+
+public class MenuScreenView {
+
+
+}
