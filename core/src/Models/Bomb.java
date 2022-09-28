d3785ef8c8718de416611918ad09dbd6038d1441
LovisaJo <75250064+LovisaJo@users.noreply.github.com>
2022-09-28 10:43:19 +0200

    Lovisa - updated mdoel for bomb

--- core/src/Models/Bomb.java
+++ core/src/Models/Bomb.java
@@ -1,4 +1,42 @@
 package Models;
 
-public class Bomb extends Entity{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bomb extends Entity implements Runnable{
+
+    private Position position;
+    private int bombLength;
+
+    private List<Bomb> bombs = new ArrayList<>(1);
+
+
+    public Bomb(Position position, int length){
+        super();
+        this.bombLength = length;
+        run();
+
+    }
+
+    private void detonate(){
+        BombExplosion bombExplosion = new BombExplosion(position, bombLength);
+
+    }
+
+    @Override
+    public void run(){
+        boolean run = true;
+        while(run){
+            try{
+                Thread.sleep(3000);
+                detonate();
+                run = false;
+            } catch (InterruptedException e){
+                e.printStackTrace();
+                run = false;
+            }
+        }
+    }
+
+
 }
