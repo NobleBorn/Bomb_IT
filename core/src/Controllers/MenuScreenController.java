0508447bfbf0d2d0bd472f1d2918d281240e6bb8
NobleBorn <00mojali.afg@gmail.com>
2022-09-28 11:39:06 +0200

    Updating

--- core/src/Controllers/MenuScreenController.java
+++ core/src/Controllers/MenuScreenController.java
@@ -1,8 +1,88 @@
 package Controllers;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.*;
 import Views.*;
+import Models.*;
 
-public class MenuScreenController {
-    private MainMenuScreen mainMenuScreen;
+public class MenuScreenController implements Screen {
+    final Main game;
+
     private MenuScreenView menuScreenView;
+
+    private Stage stage;
+    private OrthographicCamera camera;
+
+    Text_button play_button = new Text_button("Play", 2,1, 7, 3);
+
+    public MenuScreenController(final Main game){
+        this.game = game;
+        this.stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(this.stage);
+
+        this.camera = new OrthographicCamera();
+        this.camera.setToOrtho(false, 900, 600);
+
+        menuScreenView = new MenuScreenView(game, this);
+
+        this.play_button.getButton().addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new Boot(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        this.stage.addActor(play_button.getButton());
+
+    }
+
+    public Text_button getPlay_button() {
+        return play_button;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public OrthographicCamera getCamera() {
+        return camera;
+    }
+
+    @Override
+    public void render(float delta) {
+        menuScreenView.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
--- core/src/Views/MenuScreenView.java
+++ core/src/Views/MenuScreenView.java
@@ -1,7 +1,65 @@
 package Views;
+import Controllers.MenuScreenController;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.*;
 
-public class MenuScreenView {
+public class MenuScreenView implements Screen {
+    final Main game;
+
+    private MenuScreenController menuController;
+
+
+
+    public MenuScreenView(final Main game, MenuScreenController controller){
+        this.game = game;
+        game.batch = new SpriteBatch();
+        this.menuController = controller;
+        menuController.getPlay_button().create();
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        menuController.getCamera().update();
+        game.batch.setProjectionMatrix(menuController.getCamera().combined);
+
+        game.batch.begin();
+        game.batch.end();
+
+        menuController.getStage().act();
+        menuController.getStage().draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 
 
 }
