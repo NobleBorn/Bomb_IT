b18bc2bc22855f1f0616f9c1f0d935f1f1d204b7
Snapz <max.e.levin@gmail.com>
2022-10-11 11:30:35 +0200

    Merge Lovisa CollisionChecker and continued deleting BombExplosionSquare class

--- core/src/Models/CollisionChecker.java
+++ core/src/Models/CollisionChecker.java
@@ -19,22 +19,16 @@ public class CollisionChecker {
         return tiles[x][y].isTileEmpty(); //returns whether tile is empty or not
     }
 
-
-    //Det utkommenterade gör istället att vi returnar en lista med positioner ist för true/false, om vi ist vill ha positioner
     public boolean bombCollision(BombExplosion bombExplosion) {
 
         Tile[][] tiles = map.getTiles();
-        List<BombExplosionSquare> explosionSquares = bombExplosion.getBombExplosionSquares();
-
-        //List<Position> collisionPositions = new ArrayList<>(1);
+        List<Position> explosionSquares = bombExplosion.getBombExplosionSquares();
 
         for (int i = 0; i <= bombExplosion.getBombExplosionSquares().size(); i++) {
-            if (!(tiles[explosionSquares.get(i).getPosition().getX()][explosionSquares.get(i).getPosition().getY()].isTileEmpty())) {
+            if (!(tiles[explosionSquares.get(i).getX()][explosionSquares.get(i).getY()].isTileEmpty())) {
                 return true;
-                //collisionPositions.add(new Position(explosionSquares.get(i).getPosition().getX(),explosionSquares.get(i).getPosition().getY()));
             }
         }
         return false;
-        //return collisionPositions;
     }
 }
